8c70902d764804028497c0c58870ca2b50e7b218
Gladys Nduku Mitambo <gladysmitambo@gmail.com>
2022-03-14 05:20:18 -0700

    My puts task commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#<include stdio.h>
+/**
+ *main -entry pointg
+ *puts function
+ *return 0 after printing output
+*/
+int main(void)
+{
+puts("Programming is ike buiding a multilingual puzzle");
+return (0);
+}
