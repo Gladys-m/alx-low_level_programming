bc08cb12f9045d333687dba5c86b08e45136fbba
Gladys-m <gladysmitambo@gmail.com>
2022-06-02 16:53:25 -0700

    printing with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints a string using the function puts
- * 
+ * main - Prints a string using the function puts 
  * Return: (0) after printing output
  */
 int main(void)
