3811fedcf4494e8c9b0d7592c8cb7e5dd0267efa
Gladys-m <gladysmitambo@gmail.com>
2022-06-02 18:48:56 -0700

    Printing to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints a quote in the standard error
+ * Return: 1 after printing to standard error
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return 1;
+}
