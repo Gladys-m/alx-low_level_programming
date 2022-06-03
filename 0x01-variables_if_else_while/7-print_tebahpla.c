1c17e1fd10b65bd2cfe87393eff55bb4307961f2
Gladys-m <gladysmitambo@gmail.com>
2022-06-03 16:35:17 -0700

    Lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry function
+ * 
+ * Return : Always 0 (success)
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while ( char > 'a' )
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
