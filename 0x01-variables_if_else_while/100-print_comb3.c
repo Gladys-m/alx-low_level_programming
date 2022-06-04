bea44227a40fdc692999fc2dc5c2589098bbf172
Gladys-m <gladysmitambo@gmail.com>
2022-06-03 19:42:26 -0700

    Two digit combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints different combinations of two digits
+ * return: 0 upon success
+ */
+
+int main (void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i<10 ; i++)
+	{
+		for (j = 0; j<10; j++)
+		{
+			printf("%d, %d", i, j);
+		}
+	}
+	return (0);
+}
