ecba81c53f4d3e85b410772a46c372c98b647e4e
Gladys-m <gladysmitambo@gmail.com>
2022-06-03 19:44:44 -0700

    Two digit combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,14 +4,14 @@
  * return: 0 upon success
  */
 
-int main (void)
+int main(void)
 {
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; i<10 ; i++)
+	for (i = 0; i < 10 ; i++)
 	{
-		for (j = 0; j<10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			printf("%d, %d", i, j);
 		}
