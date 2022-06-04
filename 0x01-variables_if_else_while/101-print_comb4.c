1182468b08ab7667fd3a93ca07dd95d24ec585af
Gladys-m <gladysmitambo@gmail.com>
2022-06-03 19:58:25 -0700

    Three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints combination of three numbers
+ * Return: (0) upon successful output
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	for (i = 0; i<10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				printf("%d%d ,%d", i, j, k);
+			}
+		}
+	}
+	return (0);
+}
