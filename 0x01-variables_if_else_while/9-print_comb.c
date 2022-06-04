05bb6b0c20da0dff2aaa2cbbc13b7b04cbd3ffc7
Gladys-m <gladysmitambo@gmail.com>
2022-06-03 19:20:30 -0700

    Single digit combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,10 @@
 int main(void)
 {
 	int i = 0;
-	int j = 0;
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
-		{
-			printf("%d, %d", i, j);
-		}
+		printf("%d, ", i);
 	}
 	return (0);
 }
