9eb69e3d8e4b57bfd2352e938a212a8f85ddd360
Gladys-m <gladysmitambo@gmail.com>
2022-06-06 12:11:39 -0700

    Hexadecimal task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,13 +6,12 @@
 
 int main(void)
 {
-	char num[16] = {'0X00','0X01','0X02','0X03','0X04','0X05','0X06','0X07','0X08','0X09','0X0A','0X0B','0X0C','0X0D','0X0E','0X0F'};
-	int i;
-	int j;
+	int num;
 
-	for(i = 0; i < 16; i++)
+	while(num < 0x10)
 	{
-		putchar(num[i]);
-	}I
+		printf("%x",num);
+		num++;
+	}
 	return (0);
 }
