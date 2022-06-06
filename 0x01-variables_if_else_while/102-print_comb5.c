b43efecc87f68d0d9c8e52ba9b90b6ac05689318
Gladys-m <gladysmitambo@gmail.com>
2022-06-06 16:55:01 -0700

    Two two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * main - Prints two two digit numbers
+ * Return: (0) after successful output
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int y;
+
+	for (i = 48; i < 58; i++)
+	{
+		for ( j = 48; j < 57; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (y = 49; y < 58; y++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(y);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	return (0);
+}
