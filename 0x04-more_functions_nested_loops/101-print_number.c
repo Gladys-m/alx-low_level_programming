5b90448762aed497810e79a32c5bf0c6db52d969
Gladys-m <gladysmitambo@gmail.com>
2022-06-10 05:28:32 -0700

    Prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,7 +5,7 @@
  */
 void print_number(int n)
 {
-	if(n > 0)
+	if(n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
@@ -14,7 +14,7 @@ void print_number(int n)
 		_putchar('0');
 	else
 	{
-		if(n / 10)
+		if((n / 10) > 0)
 			print_number(n / 10);
 			_putchar(n % 10 + '0');
 	}
