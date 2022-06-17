a5782afb9ce33451fadd2cbdf4995a0989a7c131
Gladys-m <gladysmitambo@gmail.com>
2022-06-16 17:45:17 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * leet - Encodes string into leet
+ * @s: string to leet
+ *
+ * Return: return the modified string
+ */
+char *leet(char *s)
+{
+	int i;
+
+	for(i = 0; s[i] != '\0'; i++)
+	{
+		if(s[i] == 'A' || s[i] == 'a')
+		{
+			s[i] = '4';
+		}
+		else if(s[i] == 'E' || s[i] == 'e')
+		{
+			s[i] = '3';
+		}
+		else if(s[i] == 'O' || s[i] == 'o')
+		{
+			s[i] = '0';
+		}
+		else if(s[i] == 'T' || s[i] == 't')
+		{
+			s[i] = '7';
+		}
+		else if(s[i] == 'L' || s[i] == 'l')
+			s[i] = '1'
+	}
+	return (s);
+}	
