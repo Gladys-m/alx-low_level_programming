33293f91811038f481398bc6d5cf6a851634c0fd
Gladys-m <gladysmitambo@gmail.com>
2022-06-28 10:58:18 -0700

    array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,20 +12,19 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	if(size == 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
-	
 	ptr = (char *) malloc(size * sizeof(char));
 
-	if(ptr == 0)
+	if (ptr == 0)
 	{
 		return (NULL);
 	}
 	else
 	{
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			*(ptr + i) = c;
 		}
