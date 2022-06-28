54137c53a584358f8870daa9de5d0031e5a36db4
Gladys-m <gladysmitambo@gmail.com>
2022-06-28 09:48:15 -0700

    Create an array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: char to be put in array
+ *
+ * Return: pointer to array or NULL if fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if(size == 0)
+	{
+		return (NULL);
+	}
+	
+	ptr = malloc(size * sizeof(char));
+
+	if(ptr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for(i = 0; i < size; i++)
+		{
+			*(ptr + i) = c;
+		}
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Difference between dynamic and automatic allocation What is malloc and free and how to use them Why and when to use malloc
