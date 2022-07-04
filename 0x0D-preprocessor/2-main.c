dc3f46602226a2263f8e9c57e707ff17e8250234
Gladys-m <gladysmitambo@gmail.com>
2022-07-04 13:14:34 -0700

    macros and function like macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s", _FILE_);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+What are macros How to use them The most common predifined macros How to include guard in header files
