8bf9bc934439f447e9708dc2449c4037e07ccace
Gladys-m <gladysmitambo@gmail.com>
2022-07-08 18:48:19 -0700

    function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+
+/**
+ * op_add - finds sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of the two integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - finds difference of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: difference of the two numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - finds product of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: product of the two numbers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - finds modulus of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
