3afd9d4c1ebf0ab1ffc6e8758a26b6e2933aace1
Gladys-m <gladysmitambo@gmail.com>
2022-07-19 12:38:55 -0700

    free linked list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head node
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+	listint_t *aux;
+
+	while (temp != NULL)
+	{
+		aux = temp;
+		temp = temp->next;
+		free(aux);
+	}
+	*head = NULL;
+}
