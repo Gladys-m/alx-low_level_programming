978e05c42c900f8e0afd479e720132b20cdddf40
Gladys-m <gladysmitambo@gmail.com>
2022-07-19 15:26:24 -0700

    find nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,14 +11,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp = head;
 	unsigned int count = 0;
-	
-	while (temp != NULL)
+
+	if (!head)
+		return (NULL);
+
+	while (count < index)
 	{
-		if (count == index)
-			return (temp);
 		count++;
 		temp = temp->next;
 	}
 	
-	assert(0);
+	return (temp);
 }
