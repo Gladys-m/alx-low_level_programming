b21875366c0c5c965546f263814c6b1f036891fb
Gladys-m <gladysmitambo@gmail.com>
2022-07-20 18:40:25 -0700

    inserting node at nth index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,6 +12,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp = *head;
 	unsigned int i = 0;
+	unsigned int count;
 	listint_t *newnode;
 
 	newnode = malloc(sizeof(listint_t));
@@ -20,8 +21,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!head || !newnode)
 		return (NULL);
+	
+	count = listint_len(temp);
 
-	if (idx == 0)
+	if (idx > count)
+		return (NULL);
+
+	else if (idx == 0)
 	{
 		newnode->next = *head;
 		*head = newnode;
@@ -38,5 +44,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp->next = newnode;
 		return (newnode);
 	}
-	return (NULL);
 }
