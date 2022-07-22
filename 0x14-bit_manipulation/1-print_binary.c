be8006af9bed071f941667bf4cfcec31369b4337
Gladys-m <gladysmitambo@gmail.com>
2022-07-22 15:41:51 -0700

    binary manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_binary - prints binary of a number
+ * @n: number to be converted
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int var;
+
+	for (i = 63; i >= 0; i--)
+	{
+		var = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
