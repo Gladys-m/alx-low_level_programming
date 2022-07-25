4f66ace7009aeedc3911b866de8e373819a991ed
Gladys-m <gladysmitambo@gmail.com>
2022-07-25 11:43:18 -0700

    file descriptors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t fd, fdread, fdwrite;
-	char *lettersize;
+	char *buffer;
 
 	buffer = malloc(sizeof(char) * letters);
 	if (filename == NULL)
