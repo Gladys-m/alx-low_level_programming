1ac0b66dc6f5c2098bedafb0c8422b1f7192f513
Gladys-m <gladysmitambo@gmail.com>
2022-07-25 11:50:23 -0700

    file descriptors

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  *
  * Returns: 1 on success
  */
-int craete_file(const char *filename, char *text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int fd, fdwrite;
         int count = 0;
