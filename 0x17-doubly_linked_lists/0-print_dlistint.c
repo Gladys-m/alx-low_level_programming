1b512207d2cf531cb1200d35392c113541738b6f
Gladys-m <gladysmitambo@gmail.com>
2022-09-01 11:12:18 -0700

    displaying a doubly linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,8 +15,8 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (temp != NULL)
 	{
-		printf("%d", temp -> n);
-		temp = temp -> next;
+		printf("%d\n", temp -> n);
+		temp = temp->next;
 		count++;
 	}
 
