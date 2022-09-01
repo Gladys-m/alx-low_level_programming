db9ee03db39d080dcffd4adac805b0bede0e3ae5
Gladys-m <gladysmitambo@gmail.com>
2022-09-01 11:03:14 -0700

    displaying a doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints elements of a doubly linked list
+ * @h: head pointer
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		printf("%d", temp -> n);
+		temp = temp -> next;
+		count++;
+	}
+
+	return (count);
+}
